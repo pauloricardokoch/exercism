8dd1ede5f31abea656b829fa419d5c60ddbf38ed
Paulo Koch <pkoch@debian.pkoch>
2022-01-29 08:01:02 -0300

    git gud

---
+++ go/bird-watcher/bird_watcher.go
@@ -0,0 +1,31 @@
+package birdwatcher
+
+// TotalBirdCount return the total bird count by summing
+// the individual day's counts.
+func TotalBirdCount(birdsPerDay []int) int {
+	var total = 0
+	for _, number := range birdsPerDay {
+		total += number
+	}
+
+	return total
+}
+
+// BirdsInWeek returns the total bird count by summing
+// only the items belonging to the given week.
+func BirdsInWeek(birdsPerDay []int, week int) int {
+	offset := 7 * (week - 1)
+	return TotalBirdCount(birdsPerDay[offset : offset+7])
+}
+
+// FixBirdCountLog returns the bird counts after correcting
+// the bird counts for alternate days.
+func FixBirdCountLog(birdsPerDay []int) []int {
+	for i := range birdsPerDay {
+		if i%2 == 0 {
+			birdsPerDay[i] = birdsPerDay[i] + 1
+		}
+	}
+
+	return birdsPerDay
+}
