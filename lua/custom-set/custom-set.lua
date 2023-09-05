cb6656589b4a9291e01fea4300c9c78c020bf921
Paulo Koch <paulo.koch@g.globo>
2023-09-05 00:23:04 -0300

    commit

--- lua/custom-set/custom-set.lua
+++ lua/custom-set/custom-set.lua
@@ -5,7 +5,7 @@ function Set:is_empty()
 end
 
 function Set:contains(x)
-    for i, v in ipairs(self.elements) do
+    for _, v in ipairs(self.elements) do
         if v == x then
             return true
         end
@@ -14,22 +14,82 @@ function Set:contains(x)
 end
 
 function Set:is_subset(s)
-    for i, v in ipairs(s.elements) do
-        if self:contains(v) == false then
+    if #self.elements == 0 then
+        return true
+    end
+
+    if #s.elements == 0 then
+        return false
+    end
+
+    for _, v in ipairs(self.elements) do
+        if not s:contains(v) then
             return false
         end
     end
     return true
 end
 
-function Set:is_disjoint()
-
+function Set:is_disjoint(s)
+    for _, v in ipairs(s.elements) do
+        if self:contains(v) then
+            return false
+        end
+    end
+    return true
 end
 
 function Set:new(...)
     local arg = { ... }
     self.__index = self
     return setmetatable({ elements = arg }, self)
+end
+
+function Set:union(s)
+    for _, v in ipairs(s.elements) do
+        if not self:contains(v) then
+            table.insert(self.elements, v)
+        end
+    end
+    return self
+end
+
+function Set:equals(s)
+    if #s.elements ~= #self.elements then
+        return false
+    end
+
+    for _, v in ipairs(s.elements) do
+        if not self:contains(v) then
+            return false
+        end
+    end
+    return true
+end
+
+function Set:difference(s)
+    for i = #self.elements, 1, -1 do
+        if s:contains(self.elements[i]) then
+            table.remove(self.elements, i)
+        end
+    end
+    return self
+end
+
+function Set:intersection(s)
+    for i = #self.elements, 1, -1 do
+        if not s:contains(self.elements[i]) then
+            table.remove(self.elements, i)
+        end
+    end
+    return self
+end
+
+function Set:add(x)
+    if not self:contains(x) then
+        table.insert(self.elements, x)
+    end
+    return self
 end
 
 return function(...)
